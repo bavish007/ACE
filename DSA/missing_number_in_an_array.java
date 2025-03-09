5d6f31a2c565c7807fabd9e753e11cbb52673e4d
Bavish Reddy Muske <bavish2005@gmail.com>
2025-03-09 21:45:22 +0530

    missing_number_in_an_array.java

--- DSA/missing_number_in_an_array.java
+++ DSA/missing_number_in_an_array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length; // Get the length of the array
-        int expectedSum = n * (n + 1) / 2; // Calculate the expected sum of numbers from 0 to n
-        int actualSum = 0; // Initialize the actual sum of elements in the array
+        int n = nums.length; // length of the array
+        int expectedSum = n * (n + 1) / 2; // expected sum of numbers from 0 to n
+        int actualSum = 0; 
         
-        // Calculate the actual sum of the elements in the array
+        // Calculates actual sum of the elements in the array
         for (int num : nums) {
             actualSum += num;
         }
         
-        // The missing number is the difference between the expected sum and the actual sum
+        // missing number is the diff b.n the expected sum & the actual sum
         return expectedSum - actualSum;
     }
 }
